f03daf7a0cafad73a02a14a58d1da1cdeba9ccb3
Emarroquin1 <Erickmarroquin503@hotmail.com>
2023-03-12 18:34:32 -0600

    final app

--- Ejercicio1Semana2/app/src/main/java/com/example/ejercicio1semana2/MainActivity.java
+++ Ejercicio1Semana2/app/src/main/java/com/example/ejercicio1semana2/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.ejercicio1semana2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -16,27 +19,59 @@ public class MainActivity extends AppCompatActivity {
 AutoCompleteTextView txtSelectAutoComplete;
 ArrayAdapter<String> adapterItems;
 
+
+    Button btnEnviarInfo;
+    EditText txtDui, txtNombre,txtValorHoras;
+    Integer horasItem=0;
+    Double salario=1.0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         for(int i=0;i<110;i++) {
 
             horas[i] = "" + (i+1);
         }
-            txtSelectAutoComplete = findViewById(R.id.txtSelectAutoComplete);
+        btnEnviarInfo = findViewById(R.id.btnEnviar);
+        txtSelectAutoComplete = findViewById(R.id.txtSelectAutoComplete);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_item, horas);
+        txtSelectAutoComplete.setAdapter(adapterItems);
 
-            adapterItems = new ArrayAdapter<String>(this, R.layout.list_item, horas);
-            txtSelectAutoComplete.setAdapter(adapterItems);
+        txtSelectAutoComplete.setEnabled(false);
+
+btnEnviarInfo.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+
+
+        txtDui = findViewById(R.id.txtDUI);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtValorHoras =findViewById(R.id.txtValorHora);
+
+        Intent  info = new Intent(MainActivity.this,recibeInfo.class);
+        info.putExtra("nombre","El nombre es: "+txtNombre.getText().toString());
+        info.putExtra("dui","El Numero de DUI es: "+txtDui.getText().toString());
+        info.putExtra("horas","Las horas laboradas son: "+horasItem.toString());
+        info.putExtra("ValorHora","El valor por hora es: "+txtValorHoras.getText().toString());
+        salario = Double.parseDouble(txtValorHoras.getText().toString())*horasItem;
+        info.putExtra("salario","El salario es: "+(salario.toString()));
+
+      startActivity(info);
+    }
+});
+
 
           txtSelectAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
               @Override
               public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                  horasItem = Integer.parseInt(parent.getItemAtPosition(position).toString());
                   String item = parent.getItemAtPosition(position).toString();
                   Toast.makeText(getApplicationContext(), "Horas trabajadas: "+item,Toast.LENGTH_SHORT).show();
-
 
               }
           });
--- Ejercicio1Semana2/app/src/main/java/com/example/ejercicio1semana2/recibeInfo.java
+++ Ejercicio1Semana2/app/src/main/java/com/example/ejercicio1semana2/recibeInfo.java
@@ -2,14 +2,57 @@ package com.example.ejercicio1semana2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class recibeInfo extends AppCompatActivity {
+
+
+    Button btnRegresar, btnSalir;
+    TextView txtDui, txtNombre, txtSalario, txtHora, txtValorHora;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recibe_info);
+
+        txtDui = findViewById(R.id.txtDUI);
+        txtHora = findViewById(R.id.txtHorasLaboradas);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtValorHora = findViewById(R.id.txtValorHora);
+        txtSalario = findViewById(R.id.txtSalario);
+btnRegresar = findViewById(R.id.btnRegresar);
+btnSalir = findViewById(R.id.btnSalir);
+        Bundle bundle = getIntent().getExtras();//Variable para obtener los datos de mi anterior View
+
+        txtNombre.setText(bundle.getString("nombre"));
+        txtDui.setText(bundle.getString("dui"));
+        txtHora.setText(bundle.getString("horas"));
+        txtValorHora.setText(bundle.getString("ValorHora"));
+        txtSalario.setText(bundle.getString("salario"));
+
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regresar = new Intent(recibeInfo.this,MainActivity.class);
+                startActivity(regresar);
+            }
+        });
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
