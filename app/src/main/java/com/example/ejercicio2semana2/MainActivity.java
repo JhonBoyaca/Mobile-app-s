94eb05b757c878e0bbc36f5dee9b1183fccd4e69
Emarroquin1 <Erickmarroquin503@hotmail.com>
2023-03-13 21:52:08 -0600

    fin update

--- app/src/main/java/com/example/ejercicio2semana2/MainActivity.java
+++ app/src/main/java/com/example/ejercicio2semana2/MainActivity.java
@@ -13,12 +13,13 @@ public class MainActivity extends AppCompatActivity {
     Button btnAgregar, btnProcesar;
     EditText txtNombre, txtNota;
     TextView txtRespuesta; int count=0;
+    Double acum=0.0,promedio=0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        double[] nota = new double[4];
+        double[] nota = new double[3];
 
 
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         btnProcesar = findViewById(R.id.btnProcesar);
         txtNota = findViewById(R.id.txtNota);
         txtRespuesta = findViewById(R.id.txtRespuesta);
+        txtNombre = findViewById(R.id.txtNombre);
 
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +52,25 @@ public class MainActivity extends AppCompatActivity {
                 if(count>=2){
 
                     Intent data = new Intent(MainActivity.this,showInfo.class);
+                    data.putExtra("nombre",txtNombre.getText().toString());
 
+                    for(int i=0; i<=2;i++ ){
+                        Double notaString;
+
+                        notaString = nota[i];
+
+                            data.putExtra("Nota"+(i+1),"La nota "+(i+1)+" es: "+notaString.toString()+" ");
+
+                        acum+=nota[i];
+                    }
+                    promedio = acum/3;
+                    if(promedio>=7){
+                        data.putExtra("promedio","El promedio es: "+promedio.toString()+"\nPaso la materia");
+                    }else if (promedio<7){
+                        data.putExtra("promedio","El promedio es: "+promedio.toString()+"\nDejo la materia");
+                    }
+
+                    startActivity(data);
 
                 }else if(count ==0){
                     txtRespuesta.setText("Debe agregar las notas que se le solicitan \nNotas Restantes 3");
--- app/src/main/java/com/example/ejercicio2semana2/showInfo.java
+++ app/src/main/java/com/example/ejercicio2semana2/showInfo.java
@@ -1,13 +1,58 @@
 package com.example.ejercicio2semana2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class showInfo extends AppCompatActivity {
-
+    String notas="";
+    TextView txtNotas,txtNombre,txtPromedio;
+    Button btnSalir, btnRegresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_info);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtNotas = findViewById(R.id.txtNotas);
+        txtPromedio = findViewById(R.id.txtPromedio);
+        btnRegresar = findViewById(R.id.btnRegresar);
+        btnSalir = findViewById(R.id.btnSalir);
+
+Bundle bundle = getIntent().getExtras();
+
+txtNombre.setText(bundle.getString("nombre"));
+notas+=(bundle.getString("Nota1"));
+notas+=(bundle.getString("Nota2"));
+notas+=(bundle.getString("Nota3"));
+txtNotas.setText(notas);
+txtPromedio.setText(bundle.getString("promedio"));
+
+btnSalir.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+});
+
+btnRegresar.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+        Intent regresar = new Intent(showInfo.this,MainActivity.class);
+        startActivity(regresar);
+
+    }
+});
+
+
+
     }
 }
